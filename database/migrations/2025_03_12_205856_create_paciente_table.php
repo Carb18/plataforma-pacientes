f317fb75c616cb3d54a50a3add06ffe001a57dd4
Camilo Ruiz <camilofj21@gmail.com>
2025-03-12 16:06:47 -0500

    Creando y definiendo migraciones

---
+++ database/migrations/2025_03_12_205856_create_paciente_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('paciente', function (Blueprint $table) {
+            // Definiendo los campos y llaves foraneas de la tabla paciente.
+            $table->id();
+            $table->foreignId('tipo_documento_id')->constrained('tipos_documeto');
+            $table->string('numero_documeto');
+            $table->string('nombre1');
+            $table->string('nombre2')->nullable();
+            $table->string('apellido1');
+            $table->string('apellido2')->nullable();
+            $table->foreignId('genero_id')->constrained('genero');
+            $table->foreignId('departamento_id')->constrained('departamentos');
+            $table->foreignId('municipio_id')->constrained('municipios');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('paciente');
+    }
+};
